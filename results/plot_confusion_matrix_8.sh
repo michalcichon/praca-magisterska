6fd48961d7e52d1938f05526df907b132011025f
Michal Cichon <michal.cichon@aol.com>
2014-10-17 18:14:21 +0200

    Confusion matrixes

---
+++ results/plot_confusion_matrix_8.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/gnuplot
+
+reset
+
+set pm3d map
+
+set encoding utf8
+
+set autoscale fix
+set palette defined (0 'white', 1 'green')
+
+set terminal png size 640,480 
+set output 'confusion-matrix-8.png'
+
+set tics scale 0
+unset cbtics
+set cblabel 'Dokladnosc'
+unset key
+
+set xtics 1
+set ytics 1
+
+set title "Dokładność dla 8 kategorii"
+
+plot 'confusion_matrix_8.txt' matrix with image,\
+     '' matrix using 1:2:(sprintf('%.3f', $3)) with labels font ',10'
